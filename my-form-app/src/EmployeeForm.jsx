4c82197c668e6989db9d6f2d947ee0469d8cc31d
rashmisathe684 <rashmisathe684@gmail.com>
2026-05-13 12:37:51 +0530

    Added CSS to the Employee Form

--- my-form-app/src/EmployeeForm.jsx
+++ my-form-app/src/EmployeeForm.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import "./App.css";
 
 const EmployeeForm = () => {
   let [empform, setEmpform] = useState({
@@ -8,22 +9,27 @@ const EmployeeForm = () => {
     salary: "",
   });
 
+  const [showAlert, setShowAlert] = useState(false);
+
   const handleSubmit = (e) => {
     e.preventDefault();
+
+    setShowAlert(true);
+
     console.log(empform);
-    setEmpform({ id: "", empname: "", role: "", salary: "" });
+
+    setEmpform({
+      id: "",
+      empname: "",
+      role: "",
+      salary: "",
+    });
   };
 
   const handleChanges = (e) => {
     let { name, value } = e.target;
-    setEmpform({ ...empform, [name]: value });
-  };
 
-  const handleClick = () => {
-    console.log(empform.id);
-    console.log(empform.empname);
-    console.log(empform.role);
-    console.log(empform.salary);
+    setEmpform({ ...empform, [name]: value });
   };
 
   return (
@@ -31,46 +37,67 @@ const EmployeeForm = () => {
       <center>
         <form onSubmit={handleSubmit}>
           <h2>Employee Form</h2>
-          Enter Your id :{" "}
+
+          Enter Your id :
           <input
             type="text"
             name="id"
             value={empform.id}
             onChange={handleChanges}
           />
+
           <br />
           <br />
-          Enter Your name :{" "}
+
+          Enter Your name :
           <input
             type="text"
             name="empname"
             value={empform.empname}
             onChange={handleChanges}
           />
+
           <br />
           <br />
-          Enter Your role :{" "}
+
+          Enter Your role :
           <input
             type="text"
             name="role"
             value={empform.role}
             onChange={handleChanges}
           />
+
           <br />
           <br />
-          Enter Your salary :{" "}
+
+          Enter Your salary :
           <input
             type="text"
             name="salary"
             value={empform.salary}
             onChange={handleChanges}
           />
+
           <br />
           <br />
+
           <button type="submit">Add Employee</button>
         </form>
 
-        {/* <button type="button" onClick={handleClick}>Display Data to Console</button> */}
+        {/* Custom Alert Box */}
+        {showAlert && (
+          <div className="alert-overlay">
+            <div className="alert-box">
+              <h2>✅ Employee Added</h2>
+              <p>Employee added successfully!</p>
+
+              <button onClick={() => setShowAlert(false)}>
+                OK
+              </button>
+            </div>
+          </div>
+        )}
       </center>
     </div>
   );
